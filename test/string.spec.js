9607ef9f0cc1c0293b0af1e95e73344fc1e6ce6a
Willem (W.F.) Veelenturf <willem.veelenturf@ing.nl>
2016-10-24 15:33:02 +0200

    Fix allmost all tests

--- test/string.spec.js
+++ test/string.spec.js
@@ -8,23 +8,22 @@ var string = require('../src/string');
 describe('string', function () {
 
 
-    it('should single string', function (done) {
-        var val = 'test'
-        var res = string(val)
-        assert.deepEqual(res, {$$str$$: 'test'});
-        done();
+    it('should single string', function () {
+        var val = 'test';
+        var res = string(val);
+        assert.deepEqual(res, {
+            $$str$$: 'test'}
+        );
     });
 
-    it('should pojo', function (done) {
+    it('should pojo', function () {
         var val = {
             test: 'test'
         };
-        var res = string(val)
+        var res = string(val);
         assert.deepEqual(res, {
             test: {$$str$$: 'test'}
         });
-        done();
     });
-
 
 });
