12e1db98c97d6775cff5667998e32630e29c6ae8
Willem (W.F.) Veelenturf <willem.veelenturf@ing.nl>
2017-01-03 22:20:40 +0100

    Add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+var path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+
+    output: {
+        path: path.resolve(__dirname, "assets"),
+        filename: "bundle.js",
+        library: "cmacc"
+    },
+    node: {
+        fs: "empty",
+        request: "empty"
+    },
+    externals: {
+        "request": "notexist"
+    },
+    module: {
+        loaders: [
+            { test: /\.json/, loader: 'json' }
+        ]
+    },
+};
