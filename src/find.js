2058a4c1e0c3211afd5292184699418f2880f2c3
Willem Veelenturf <wveelenturf@bol.com>
2017-02-13 16:03:25 +0100

    Add find

---
+++ src/find.js
@@ -0,0 +1,21 @@
+function find(ast) {
+    var res = {};
+
+    var keys = Object.keys(ast);
+
+    for (var i = 0; i < keys.length; i++) {
+
+        var key = keys[i];
+
+        if (!key.match(/\$\$(.*)\$\$/) && !ast[key])
+            res[key] = "";
+
+        if (!key.match(/\$\$(.*)\$\$/) && ast[key] && !ast[key].$$str$$)
+            res[key] = findEmptyVars(ast[key])
+
+    }
+
+    return res;
+}
+
+module.exports = find;
