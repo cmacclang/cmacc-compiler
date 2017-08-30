ab9e08dadc1f38c8e26d567e9a6c5c77ea6b8a87
Willem Veelenturf <wveelenturf@bol.com>
2017-08-30 14:44:07 +0200

    Add two more test cases for variable in variable

---
+++ test/cases/variable_in_string/test.js
@@ -0,0 +1,30 @@
+const url = require('path');
+const assert = require('assert');
+const cmacc = require('../../../src/index');
+
+describe('variable_in_string', function () {
+
+  global.fs = require('fs');
+  global.fetch = require('node-fetch');
+
+  it('Test1', function (done) {
+    const file = url.join('file://', __dirname, './Example.cmacc');
+
+    cmacc.compile(file)
+      .then(ast => {
+        return ast;
+      })
+      .then(cmacc.render)
+      .then(x => {
+        return cmacc.remarkable.render(x)
+      })
+      .then(html => {
+        const expect = `Test2 Test1`;
+        assert.equal(html, expect);
+        done();
+      })
+      .catch(done);
+
+  });
+
+});
