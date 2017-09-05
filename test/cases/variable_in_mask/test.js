21a1965bb3de8ff2db72ab9fd491826857797729
Willem Veelenturf <wveelenturf@bol.com>
2017-09-05 08:30:59 +0200

    More tests

---
+++ test/cases/variable_in_mask/test.js
@@ -0,0 +1,31 @@
+const url = require('path');
+const assert = require('assert');
+const cmacc = require('../../../src/index');
+
+describe('variable_in_string', function () {
+
+  global.fs = require('fs');
+  global.fetch = require('node-fetch');
+
+  it('Test1', function (done) {
+    const file = url.join('file://', __dirname, './Example.cmacc');
+
+    cmacc.compile(file)
+      .then(ast => {
+        return ast;
+      })
+      .then(cmacc.render)
+      .then(x => {
+        return cmacc.remarkable.render(x)
+      })
+      .then(html => {
+        const expect = `<p>Hello my name is Willem Veelenturf</p>
+`;
+        assert.equal(html, expect);
+        done();
+      })
+      .catch(done);
+
+  });
+
+});
